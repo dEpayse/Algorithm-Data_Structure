326663bd6364e92667b34a387b5e1b8580b5d872
dEpayse <56813199+dEpayse@users.noreply.github.com>
2021-07-27 00:37:35 +0900

    [ADD] 동적 계획법, 분할 정복 비교 예시
    피보나치 수열 예시를 이용

---
+++ algorithm/src/Fibonacci.kt
@@ -0,0 +1,39 @@
+import java.util.*
+
+fun main() {
+    // 입력값이 클수록 분할 정복(재귀 함수)에 걸리는 시간이 기하 급수적으로 오래 걸릴 것임.
+    val scanner = Scanner(System.`in`)
+    println("피보나치 수열의 몇 번째 값이 궁금하신가요?(숫자만 입력해주세요)")
+    val num = scanner.nextInt()
+    println("------------------------------------------")
+    var beforeTime = System.currentTimeMillis()
+    println("동적 계획법 결과값 : ${fibonacciDynamicProgramming(num)}")
+    var afterTime = System.currentTimeMillis()
+    println("걸린 시간 : ${afterTime-beforeTime} ms")
+    println("------------------------------------------")
+    beforeTime = System.currentTimeMillis()
+    println("분할 정복 결과값: ${fibonacciDivideConquer(num)}")
+    afterTime = System.currentTimeMillis()
+    println("걸린 시간 : ${afterTime-beforeTime} ms")
+    println("------------------------------------------")
+}
+
+// 동적 계획법(Dynamic Programming) - memoization 기법을 사용함
+fun fibonacciDynamicProgramming(num: Int): Int {
+    if (num <= 1) return num
+    val fiboList = IntArray(num + 1)
+    fiboList[0] = 0
+    fiboList[1] = 1
+    for (i in 2..num) {
+        fiboList[i] = fiboList[i - 1] + fiboList[i - 2]
+    }
+    return fiboList[num]
+}
+
+// 분할 정복 - 계산 중복을 고려하지 않고 재귀 함수를 사용함 => 중복이 많음
+fun fibonacciDivideConquer(num: Int): Int {
+    // base case, termination case
+    if (num <= 1) return num
+    // recursive case
+    return fibonacciDivideConquer(num - 1) + fibonacciDivideConquer(num - 2)
+}
